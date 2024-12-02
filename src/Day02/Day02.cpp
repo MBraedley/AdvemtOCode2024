536d3392ba573dbd50446c604f9dc66c47946f26
Michael Braedley <mike.braedley@gmail.com>
2024-12-02 08:44:59 -0400

    Day 2 complete

--- src/Day02/Day02.cpp
+++ src/Day02/Day02.cpp
@@ -6,15 +6,66 @@
 
 #include "ctre.hpp"
 
+#include <ranges>
+
+bool CheckReport(const std::vector<int>& levels)
+{
+	std::vector<int> diffs;
+	std::adjacent_difference(levels.begin(), levels.end(), std::back_inserter(diffs));
+
+	auto [minIt, maxIt] = std::minmax_element(std::next(diffs.begin()), diffs.end());
+	return *minIt >= 1 && *maxIt <= 3;
+}
+
 int main()
 {
 	auto startTime = std::chrono::system_clock::now();
 
 	std::filesystem::path inputFile("../Day02_input.txt");
+	auto lines = utils::ReadInput(inputFile);
 
-	constexpr utils::RegexStr re{ R"((\d+) (\d+))" };
+	std::vector<std::vector<int>> report;
+	for (const std::string& line : lines)
+	{
+		std::stringstream sstrm(line);
+		int i;
+		std::vector<int> levels;
+		while (sstrm >> i)
+		{
+			levels.push_back(i);
+		}
 
-	std::vector<std::vector<std::string>> input = utils::ReadFormattedInput<re>(inputFile);
+		report.emplace_back(levels);
+		report.emplace_back(levels.rbegin(), levels.rend());
+	}
+
+	std::size_t count1 = 0;
+	std::size_t count2 = 0;
+
+	for (const auto& levels : report)
+	{
+		if (CheckReport(levels))
+		{
+			count1++;
+			count2++;
+		}
+		else
+		{
+			for (std::size_t i = 0; i < levels.size(); i++)
+			{
+				std::vector<int> levelsCopy = levels;
+				levelsCopy.erase(levelsCopy.begin() + i);
+				if (CheckReport(levelsCopy))
+				{
+					count2++;
+					break;
+				}
+			}
+		}
+	}
+
+	utils::PrintResult(count1, startTime);
+	utils::PrintResult(count2, startTime);
 
 	return 0;
 }
