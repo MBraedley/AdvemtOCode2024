38fb59247e96232c3f31535e72b3e1a851ae5473
Michael Braedley <mike.braedley@gmail.com>
2024-12-03 09:11:57 -0400

    Day 3 part 1

--- src/Day03/Day03.cpp
+++ src/Day03/Day03.cpp
@@ -6,5 +6,22 @@
 
 int main()
 {
+	auto startTime = std::chrono::system_clock::now();
+
+	auto regex = ctre::range<R"(mul\((\d{1,3}),(\d{1,3})\))">;
+
+	auto input = utils::ReadInput("../Day03_input.txt");
+
+	std::uint64_t sum = 0;
+	for (std::string line : input)
+	{
+		for (auto re: regex(line))
+		{
+			sum += re.get<1>().to_number(10) * re.get<2>().to_number();
+		}
+	}
+
+	utils::PrintResult(sum, startTime);
+
 	return 0;
 }
