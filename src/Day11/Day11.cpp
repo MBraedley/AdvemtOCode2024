52ca14b1e2f3e2813ea9b5a293e51257fcbf5102
Michael Braedley <mike.braedley@gmail.com>
2024-12-11 09:03:34 -0400

    Day 11 part 1, tried to brute force part 2

--- src/Day11/Day11.cpp
+++ src/Day11/Day11.cpp
@@ -4,7 +4,80 @@
 #include "Day11.h"
 #include "Utils.h"
 
+#include <list>
+#include <functional>
+
 int main()
 {
+	auto startTime = std::chrono::system_clock::now();
+
+	std::list<std::uint64_t> stones;
+
+	std::filesystem::path inputFile("../Day11_input.txt");
+
+	std::ifstream fileStrm(inputFile);
+	std::uint64_t stone;
+	while (fileStrm >> stone)
+	{
+		stones.push_back(stone);
+	}
+
+	auto shouldSplit = [](std::uint64_t stone) -> bool
+		{
+			return std::to_string(stone).size() % 2 == 0;
+		};
+
+	auto splitStone = [&](std::list<std::uint64_t>::iterator iter)
+		{
+			std::string stoneVal = std::to_string(*iter);
+			std::uint64_t first = std::stoull(stoneVal.substr(0, stoneVal.size() / 2));
+			std::uint64_t second = std::stoull(stoneVal.substr(stoneVal.size() / 2));
+			*iter = second;
+			stones.insert(iter, first);
+		};
+
+	for (std::size_t i = 0; i < 25; i++)
+	{
+		for (auto iter = stones.begin(); iter != stones.end(); iter++)
+		{
+			if (*iter == 0)
+			{
+				*iter = 1;
+			}
+			else if (shouldSplit(*iter))
+			{
+				splitStone(iter);
+			}
+			else
+			{
+				*iter *= 2024;
+			}
+		}
+	}
+
+	utils::PrintResult(stones.size(), startTime);
+
+	//Part 2
+	for (std::size_t i = 25; i < 75; i++)
+	{
+		for (auto iter = stones.begin(); iter != stones.end(); iter++)
+		{
+			if (*iter == 0)
+			{
+				*iter = 1;
+			}
+			else if (shouldSplit(*iter))
+			{
+				splitStone(iter);
+			}
+			else
+			{
+				*iter *= 2024;
+			}
+		}
+	}
+
+	utils::PrintResult(stones.size(), startTime);
+
 	return 0;
 }
