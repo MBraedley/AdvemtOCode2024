b33ccc7bc106c7386079c3cd7350c51b909de8b5
Michael Braedley <michael.braedley@cartenav.com>
2024-12-19 16:12:31 -0400

    Day 19 part 1, but I want to use ctre for it.

--- src/Day19/Day19.cpp
+++ src/Day19/Day19.cpp
@@ -4,7 +4,39 @@
 #include "Day19.h"
 #include "Utils.h"
 
+#include <ranges>
+#include <regex>	//bleh, I really want to do this with ctre
+
 int main()
 {
+	using std::operator""sv;
+	auto startTime = std::chrono::system_clock::now();
+
+	auto input = utils::ReadInput( "../Day19_input.txt" );
+
+	std::string towelsRegex = "(" + (input[0] | std::views::split( ", "sv ) | std::views::join_with( "|"sv ) | std::ranges::to<std::string>()) + ")+";
+
+	std::regex re( towelsRegex );
+
+	std::vector<std::string> towels = input | std::views::drop( 2 ) | std::ranges::to<std::vector<std::string>>();
+	std::uint32_t validCount = 0;
+
+	for( const auto& t : towels )
+	{
+		try
+		{
+			if( std::regex_match( t, re ) )
+			{
+				validCount++;
+			}
+		}
+		catch( std::regex_error e )
+		{
+			std::cout << e.what() << "\n";
+		}
+	}
+
+	utils::PrintResult( validCount, startTime );
+
 	return 0;
 }
